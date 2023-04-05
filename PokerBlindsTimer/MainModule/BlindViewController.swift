f58719c2054f90e1988e0394d21d3c494fbbf82b
kolesoking <kolesnikov.sergey.al@gmail.com>
2023-04-05 12:22:33 +0300

    1. Add MainModule 	BlindViewController 	- SettingsButton 	- SettingsButtonAction 2. Add SettingModule 	- SetingsViewController

--- PokerBlindsTimer/MainModule/BlindViewController.swift
+++ PokerBlindsTimer/MainModule/BlindViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  BlindViewController.swift
 //  PokerBlindsTimer
 //
 //  Created by Sergey Kolesnikov on 05.04.2023.
@@ -8,11 +8,51 @@
 import UIKit
 
 class BlindViewController: UIViewController {
+    
+    private var settingsButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "gear"), for: .normal)
+        button.imageView?.tintColor = .black
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.layer.cornerRadius = 25
+//        button.addTarget(BlindViewController.self, action: #selector(settingsButtonPresed), for: .touchUpInside)
+        return button
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // TODO: -
-        view.backgroundColor = .red
+//        view.backgroundColor = .white
+        
+        configureAppearance()
+        
+//        settingsButton.addTarget(self, action: #selector(settingsButtonAction), for: .touchUpInside)
+    }
+    
+    @objc func settingsButtonAction() {
+        print("OK")
     }
 }
+
+// MARK: - Private Methods
+
+private extension BlindViewController {
+    
+    func configureAppearance() {
+        view.backgroundColor = .white
+        setConstraints()
+        settingsButton.addTarget(self, action: #selector(settingsButtonAction), for: .touchUpInside)
+    }
+    
+    func setConstraints() {
+        view.addSubview(settingsButton)
+        NSLayoutConstraint.activate([
+            settingsButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            settingsButton.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            settingsButton.widthAnchor.constraint(equalToConstant: 50),
+            settingsButton.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+}
+
 
