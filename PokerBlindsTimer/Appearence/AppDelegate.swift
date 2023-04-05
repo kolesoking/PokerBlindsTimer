4c17d9981426fb77c84a0673978c121835034711
kolesoking <kolesnikov.sergey.al@gmail.com>
2023-04-05 10:31:58 +0300

    Initial Commit

---
+++ PokerBlindsTimer/Appearence/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  PokerBlindsTimer
+//
+//  Created by Sergey Kolesnikov on 05.04.2023.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = ViewController()
+        return true
+    }
+}
+
---
+++ PokerBlindsTimer/MainModule/BlindViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  PokerBlindsTimer
+//
+//  Created by Sergey Kolesnikov on 05.04.2023.
+//
+
+import UIKit
+
+class BlindViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // TODO: -
+        view.backgroundColor = .red
+    }
+}
+
